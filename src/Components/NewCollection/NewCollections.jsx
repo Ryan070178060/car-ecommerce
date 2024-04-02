f3901511835adb7fd67d8ce661554826e5e88fe9
Ryan Shamala <ryanshamala254@gmail.com>
2024-04-02 19:24:24 +0300

    tt

--- src/Components/NewCollection/NewCollections.jsx
+++ src/Components/NewCollection/NewCollections.jsx
@@ -19,12 +19,12 @@ const NewCollections = () => {
         <hr/>
        <div className="collections">
          {new_collection.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                return <Item key={i} id={item.id} name={item.name} image={`https://car-backend-tt86.onrender.com/images/${item.image}`} new_price={item.new_price} old_price={item.old_price}/>
           })}
-
         </div>
     </div>
   )
+  
 }
 
 export default NewCollections;
