ed46c7974a9fce037c16d7c6475816490e85369e
Ryan Shamala <ryanshamala254@gmail.com>
2024-04-04 21:22:17 +0300

    t

--- src/Components/NewCollection/NewCollections.jsx
+++ src/Components/NewCollection/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const [new_collection,setNew_collection] = useState([]);
 
   useEffect(()=>{
-    fetch('https://autodealer.onrender.com/allproducts').then((response)=>response.json())
+    fetch('api/allproducts').then((response)=>response.json())
     .then((data)=>setNew_collection(data))
     .catch((error) => console.error("Error fetching data:", error)); // Handle any fetch errors
   },[])
--- src/Context/ShopContext.js
+++ src/Context/ShopContext.js
@@ -8,7 +8,7 @@ const ShopContextProvider = (props) => {
     const [all_product, setAll_product] = useState([]);
 
     useEffect(() => {
-        fetch('https://autodealer.onrender.com/allproducts')
+        fetch('api/allproducts')
         .then((response) => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
