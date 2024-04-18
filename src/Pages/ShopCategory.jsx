decb04444d67877f96c61a1c0a4c1853f645f3d4
Ryan Shamala <ryanshamala254@gmail.com>
2024-04-18 18:01:37 +0300

    vvv

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import './CSS/ShopCategory.css';
 import { ShopContext } from '../Context/ShopContext';
-import dropdown_icon from '../Components/Assets/dropdown_icon.png';
+//import dropdown_icon from '../Components/Assets/dropdown_icon.png';
 import Item from '../Components/Item/Item';
 
 const ShopCategory = (props) => {
@@ -22,9 +22,9 @@ const ShopCategory = (props) => {
         <p>
           <span>Showing 1-12</span> out of 36 products
         </p>
-        <div className='shopcategory-sort'>
+      {/* <div className='shopcategory-sort'>
           Sort by <img src={dropdown_icon} alt='' />
-        </div>
+  </div>*/}
       </div>
       {loading ? ( // Display loading indicator if loading is true
         <div className='loading'>
